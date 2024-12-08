748f009b218d40e6797cdf98f0a3b53ddd0d9206
SaH1l191 <haldankarsahil98@gmail.com>
2024-12-08 18:17:21 +0530

    pathdirname added

--- backend/server.js
+++ backend/server.js
@@ -39,12 +39,12 @@ app.use("/api/v1/notifications", notificationRoutes)
 app.use("/api/v1/connections", connectionRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     // React app
     app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
 }
 
 
