c2667a662b19be43cef603486a4d716bd28b6cc7
SaH1l191 <haldankarsahil98@gmail.com>
2024-12-08 18:25:29 +0530

    cors removed

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import userRoutes from './routers/user.route.js';
 import postRoutes from './routers/post.route.js';
 import notificationRoutes from './routers/notification.route.js';
 import connectionRoutes from "./routers/connection.route.js";
-import cors from 'cors'
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import path from "path";
@@ -23,14 +22,7 @@ const app = express()
 job.start();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: "50mb" }));
-	app.use(
-		cors({
-			origin: "http://localhost:5173",
-			credentials: true,
-		})
-	); 
-app.use(express.json())
+app.use(express.json({ limit: "50mb" })); 
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users", userRoutes)
