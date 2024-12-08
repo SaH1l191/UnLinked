8f4078a0fd4016cae11f7aea8812d2831501cbe5
SaH1l191 <haldankarsahil98@gmail.com>
2024-12-08 20:19:01 +0530

    change-1

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-
-import authRoutes from "./routes/auth.route.js";
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
-import notificationRoutes from "./routes/notification.route.js";
-import connectionRoutes from "./routes/connection.route.js";
-
+ 
+import notificationRoutes from './routers/notification.route.js' 
+import connectionRoutes from './routers/connection.route.js'
+import authRoutes from './routers/auth.route.js' 
+import userRoutes from './routers/user.route.js'
+import postRoutes from './routers/post.route.js'
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
