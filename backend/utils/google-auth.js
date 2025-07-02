25ca5e0b08e307f3dc483245dd04187040c48841
SaH1l191 <haldankarsahil98@gmail.com>
2025-07-02 19:37:35 +0530

    added lazy-loading

--- backend/utils/google-auth.js
+++ backend/utils/google-auth.js
@@ -2,12 +2,14 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/user.model.js";
 
+const CLIENT_URL = process.env.CLIENT_URL;
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/api/v1/auth/google/callback",
+      callbackURL: `${CLIENT_URL}/api/v1/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,58 @@
+import React, { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/auth/LoginPage";
-import SignUpPage from "./pages/auth/SignUpPage";
 import toast, { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "./lib/axios";
-import PostPage from "./pages/PostPage";
-import NotifcationsPage from "./pages/NotifcationsPage";
-import NetworkPage from "./pages/NetworkPage";
-import ProfilePage from "./pages/ProfilePage";
-import Home from "./pages/Home";
-import NotFoundPage from "./pages/NotFound";	
-import Jobs from "./pages/Jobs";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/auth/LoginPage"));
+const SignUpPage = lazy(() => import("./pages/auth/SignUpPage"));
+const PostPage = lazy(() => import("./pages/PostPage"));
+const NotifcationsPage = lazy(() => import("./pages/NotifcationsPage"));
+const NetworkPage = lazy(() => import("./pages/NetworkPage"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+const Home = lazy(() => import("./pages/Home"));
+const NotFoundPage = lazy(() => import("./pages/NotFound"));
+const Jobs = lazy(() => import("./pages/Jobs"));
+
 function App() {
-	const { data: authUser, isLoading } = useQuery({
-		queryKey: ["authUser"],
-		queryFn: async () => {
-			try {
-				const res = await axiosInstance.get("/auth/me");
-				// console.log('logging res => auth user   = > ', res)
-				return res.data;
-			} catch (err) {
-				if (err.response && err.response.status === 401) {
-					return null;
-				}
-				toast.error(err.response.data.message || "Something went wrong");
-			}
-		},
-	});
+    const { data: authUser, isLoading } = useQuery({
+        queryKey: ["authUser"],
+        queryFn: async () => {
+            try {
+                const res = await axiosInstance.get("/auth/me");
+                return res.data;
+            } catch (err) {
+                if (err.response && err.response.status === 401) {
+                    return null;
+                }
+                toast.error(err.response.data.message || "Something went wrong");
+            }
+        },
+    });
 
-	if (isLoading) return null;
+    if (isLoading) return null;
 
-	return (
-		<Layout>
-			<Routes>
-				<Route path="*" element={<NotFoundPage/>} />
-				<Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/home"} />} />
-				<Route path='/home' element={<Home/>} />
-				<Route path='/jobs' element={authUser ? <Jobs/> : <Navigate to={"/jobs"} />} />
-				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
-				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
-				<Route path='/notifications' element={authUser ? <NotifcationsPage /> : <Navigate to={"/login"} />} />
-				<Route path='/network' element={authUser ? <NetworkPage /> : <Navigate to={"/login"} />} /> 
-				<Route path='/post/:postId' element={authUser ? <PostPage /> : <Navigate to={"/login"} />} />
-				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />} />
-			</Routes>
-			<Toaster />
-		</Layout>
-	);
+    return (
+        <Layout>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="*" element={<NotFoundPage />} />
+                    <Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/home"} />} />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/jobs' element={authUser ? <Jobs /> : <Navigate to={"/jobs"} />} />
+                    <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+                    <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+                    <Route path='/notifications' element={authUser ? <NotifcationsPage /> : <Navigate to={"/login"} />} />
+                    <Route path='/network' element={authUser ? <NetworkPage /> : <Navigate to={"/login"} />} />
+                    <Route path='/post/:postId' element={authUser ? <PostPage /> : <Navigate to={"/login"} />} />
+                    <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />} />
+                </Routes>
+            </Suspense>
+            <Toaster />
+        </Layout>
+    );
 }
 
 export default App;
