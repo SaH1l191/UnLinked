b98ac8a792e3f73f318c1df3c877c44658c38f14
SaH1l191 <haldankarsahil98@gmail.com>
2025-07-03 10:39:42 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+CLIENT_URL= "https://unlinked-ff2c.onrender.com";
+// CLIENT_URL="http://localhost:5173"
+
+axios.defaults.withCredentials = true;
+
 export const axiosInstance = axios.create({
-    baseURL: "/api/v1/", // for local dev
-    withCredentials: true,
-});
+    baseURL: `${CLIENT_URL}/api/v1/`, // for local dev
+})
