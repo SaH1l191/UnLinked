690af7273bc32982dc6451d11ccad25ac3444b80
SaH1l191 <haldankarsahil98@gmail.com>
2025-07-03 10:44:48 +0530

    change-1

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
+ 
 export const axiosInstance = axios.create({
-    baseURL: "/api/v1/", // for local dev
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
